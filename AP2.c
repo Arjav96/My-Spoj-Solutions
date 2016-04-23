8cd0c0b561ca84cc5f15d3e4d5959d1e041f8d90
Arjav Jain <Arjav96@users.noreply.github.com>
2016-04-24 01:31:28 +0530

    Create AP2.c

---
+++ AP2.c
@@ -0,0 +1,22 @@
+//fiven 3rd ,and last 3rd and sum of AP find no of terms and series.
+ 
+#include<stdio.h>
+int main()
+{
+ int t;
+ scanf("%d",&t);
+ while(t--)
+ {
+  long long c,c3,s,d,n,i,a;
+  scanf("%lld%lld%lld",&c,&c3,&s);
+  n=(2*s)/(c+c3);
+  d=(c3-c)/(n-5);
+  a=c-2*d;
+  printf("%lld\n",n);
+  for(i=0;i<n;i++)
+  {
+   printf("%lld",(a+i*d));
+   printf(" ");
+  }
+ }
+} 
