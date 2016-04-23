ede6394a2e4e297ec5a9c76bea910103fffb5819
Arjav Jain <Arjav96@users.noreply.github.com>
2016-04-24 01:46:25 +0530

    Create BITDIFF.cpp

---
+++ BITDIFF.cpp
@@ -0,0 +1,81 @@
+/* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
+ * Created By : Arjav Jain
+ * CSE, MNNIT-ALLAHABAD 
+ * vampire18
+ _._._._._._._._._._._._._._._._._._._._._.*/
+ 
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define MP make_pair
+#define pb push_back
+#define rep(i,n) for(int i=0;i<n;++i)
+#define REP(i,a,b) for(int i=a;i<=b;++i)
+#define PER(i,a,b) for(int i=b;i>=a;--i)
+#define F first
+#define S second
+#define all(c) c.begin(),c.end() //eg sort(all(v));
+ 
+#define tr(c, itr) for(itr = (c).begin(); itr != (c).end(); itr++)
+#define present(container, element) (container.find(element) != container.end()) //for set,map,etc
+#define cpresent(container, element) (find(all(container),element) != container.end()) //for vectors
+ 
+typedef long long ll;
+typedef vector<int> vi; 
+typedef vector< vi > vvi; 
+typedef pair<int,int > pii; 
+typedef pair<string,int> psi;
+ 
+#define sz(a) int((a).size())
+#define clr(a) memset(a,0,sizeof(a))
+#define ini(a) memset(a,-1,sizeof(a))
+ 
+//i/o
+#define sc(n) scanf("%d",&n)
+#define sc2(n,m) scanf("%d%d",&n,&m)
+#define scs(s) scanf("%s",s);
+#define out(n) printf("%d\n",n)
+#define out2(n,m) printf("%d %d\n",n,m)
+#define scc(n) scanf("%c",&n)
+ 
+#define scll(n) scanf("%lld",&n)
+#define scll2(n,m) scanf("%lld%lld",&n,&m)
+#define outll(n) printf("%lld\n",n)
+#define outll2(n,m) printf("%lld %lld\n",n,m)
+ 
+//cost
+#define MOD 10000007
+#define MOD_INV 1000000006
+#define MAX 10001
+#define MAX2 1000001
+ 
+#define chk(a) cerr << endl << #a << " : " << a << endl
+#define chk2(a,b) cerr << endl << #a << " : " << a << "\t" << #b << " : " << b << endl
+#define chk3(a,b,c) cerr << endl << #a << " : " << a << "\t" << #b << " : " << b << "\t" << #c << " : " << c << endl
+#define chk4(a,b,c,d) cerr << endl << #a << " : " << a << "\t" << #b << " : " << b << "\t" << #c << " : " << c << "\t" << #d << " : " << d << endl
+ 
+int arr[MAX];
+ 
+int main(){
+	int t,n,x,tmp;
+	ll ans;
+	sc(t);
+	REP(k,1,t){
+		tmp=0;ans=0;
+		sc(n);x=0;
+		rep(i,n){
+			sc(arr[i]);
+		}
+		rep(i,32){
+			tmp=0;
+			x=(1<<i);
+			rep(j,n){
+				if(arr[j] & x){
+					tmp++;
+				}
+			}
+			ans = (ans + (2*(((tmp%MOD)*((n-tmp)%MOD))%MOD)%MOD))%MOD;
+		}
+		printf("Case %d: %lld\n",k,ans);
+	}
+} 
