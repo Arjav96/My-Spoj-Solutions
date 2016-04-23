a204286cd366d7c3fe1ba41e630383d3dc326da1
Arjav Jain <Arjav96@users.noreply.github.com>
2016-04-24 01:42:48 +0530

    Create BEENUMS.c

---
+++ BEENUMS.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    while(n!=-1)
+    {
+        if(n%2)
+        {
+            n--;
+            if(n%3==0)
+            {
+                n/=3;
+                n=n*4+1;
+                int t=sqrt(n);
+                if(t*t==n)
+                {
+                    if(t%2)
+                        printf("Y\n");
+                    else
+                        printf("N\n");
+                }
+                else
+                    printf("N\n");
+            }
+            else
+                printf("N\n");
+        }
+        else
+            printf("N\n");
+        scanf("%d",&n);
+    }
+    return 0;
+}
+ 
