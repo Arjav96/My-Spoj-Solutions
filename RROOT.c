0d601c27f82d1eeda430850dc9eb785b3500ce9b
Arjav Jain <Arjav96@users.noreply.github.com>
2016-04-28 14:43:52 +0530

    Create RROOT.c

---
+++ RROOT.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<math.h>
+ 
+int main()
+{
+    int t;  double d;
+ 
+    scanf("%i",&t);
+ 
+    while(t--)
+    {
+        scanf("%lf",&d);
+ 
+        printf("%0.6lf\n", 1 - 1/(3*sqrt(d/2)));
+ 
+    }
+    return 0;
+} 
