84891cb1e1e4aadee3c1a049068a9613ebc04f4f
Arjav Jain <Arjav96@users.noreply.github.com>
2016-04-28 14:19:44 +0530

    Create PFDEP.cpp

---
+++ PFDEP.cpp
@@ -0,0 +1,99 @@
+/* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
+ * Created By : Arjav Jain
+ * CSE, MNNIT-ALLAHABAD 
+ * vampire18
+ _._._._._._._._._._._._._._._._._._._._._.*/
+ 
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define MP make_pair
+#define pb push_back
+#define rep(i,n) for(int i=0;i<n;++i)
+#define REP(i,a,b) for(int i=a;i<=b;++i)
+#define PER(i,a,b) for(int i=b;i>=a;--i)
+#define X first
+#define Y second
+#define all(c) c.begin(),c.end() //eg sort(all(v));
+ 
+#define tr(c, itr) for(itr = (c).begin(); itr != (c).end(); itr++)
+#define present(container, element) (container.find(element) != container.end()) //for set,map,etc
+#define cpresent(container, element) (find(all(container),element) != container.end()) //for vectors
+ 
+typedef long long ll;
+typedef vector<int> vi; 
+typedef vector< vi > vvi; 
+typedef pair< int,int > ii; 
+ 
+#define sz(a) int((a).size())
+#define clr(a) memset(a,0,sizeof(a))
+#define ini(a) memset(a,-1,sizeof(a))
+ 
+//i/o
+#define sc(n) scanf("%d",&n)
+#define sc2(n,m) scanf("%d%d",&n,&m)
+#define scs(s) scanf("%s",s);
+#define out(n) printf("%d\n",n)
+#define out2(n,m) printf("%d %d\n",n,m)
+#define scc(n) scanf("%c",&n)
+ 
+#define scll(n) scanf("%lld",&n)
+#define scll2(n,m) scanf("%lld%lld",&n,&m)
+#define outll(n) printf("%lld\n",n)
+#define outll2(n,m) printf("%lld %lld\n",n,m)
+ 
+//cost
+#define MOD 1000000007
+#define MOD_INV 1000000006
+#define MAX 100009
+ 
+#define chk(a) cerr << endl << #a << " : " << a << endl
+#define chk2(a,b) cerr << endl << #a << " : " << a << "\t" << #b << " : " << b << endl
+#define chk3(a,b,c) cerr << endl << #a << " : " << a << "\t" << #b << " : " << b << "\t" << #c << " : " << c << endl
+#define chk4(a,b,c,d) cerr << endl << #a << " : " << a << "\t" << #b << " : " << b << "\t" << #c << " : " << c << "\t" << #d << " : " << d << endl
+ 
+set<int> S[103], toposet;
+vector<bool> vis(103,false); 
+vector<int> V,indeg(103);
+int n,m,a,b,t;
+ 
+void topology(){
+	int start;
+	set<int>::iterator it;
+	while(!toposet.empty()){
+		start = *toposet.begin();
+		toposet.erase(toposet.begin());
+		it = S[start].begin();
+		V.pb(start);
+		while(it != S[start].end()){
+			indeg[*it]--;
+			if(!indeg[*it]) toposet.insert(*it);
+			it++;
+		}
+	}
+}
+ 
+int main(){
+	sc2(n,m);
+	rep(i,m){
+		sc(a);
+		sc(t);
+		rep(j,t){
+			sc(b);
+			S[b].insert(a);
+		}
+		indeg[a] += t;
+	}
+ 
+	REP(i,1,n){
+		if(!indeg[i])
+			toposet.insert(i);
+	}
+	
+	topology();
+	
+	rep(i,n){
+		printf("%d ",V[i]);
+	}
+	printf("\n");
+} 
