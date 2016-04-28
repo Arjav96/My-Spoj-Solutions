2735659653170e837020a72afbe841666db9f0dd
Arjav Jain <Arjav96@users.noreply.github.com>
2016-04-28 14:53:48 +0530

    Create SPCQ.cpp

---
+++ SPCQ.cpp
@@ -0,0 +1,34 @@
+// finding a number greater or equal to given which is divisible by number obtained by suming its digits.
+ 
+#include<iostream>
+#define ll long long
+using namespace std;
+int main()
+{
+ll t,n,x,m,sum;
+cin >> t;
+while(t--)
+{
+cin >> n;
+m=n;
+while(1)
+{
+m=n;
+sum=0;
+while(m>0)
+{
+x=m%10;
+sum+=x;
+m=m/10;
+}
+if(n%sum==0)
+{
+cout << n << endl;
+break;
+}
+else
+n++;
+}
+}
+return 0;
+} 
